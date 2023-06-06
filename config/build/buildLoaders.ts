2fbf3155f3526c096f78e4e778d8b22ca0a39943
ravelin9 <kvist1495@gmail.com>
2023-06-06 18:40:32 +0500

    add fsd

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,38 +1,36 @@
 import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
-import * as module from "module";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
                 loader: "css-loader",
                 options: {
                     modules: {
-                        auto: (resourcePath: string) => resourcePath.endsWith(".module.scss"),
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
                         localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:8]'
+                            ? '[path][name]__[local]--[hash:base64:5]'
                             : '[hash:base64:8]'
-                    }
-
+                    },
                 }
             },
             "sass-loader",
         ],
     }
 
-    const typescriptLoader =  {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        };
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
 
     return [
-       typescriptLoader,
+        typescriptLoader,
         cssLoader,
     ]
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,18 +1,18 @@
-import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import path from "path";
-import {BuildOptions, BuildPaths} from "./types/config";
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildPlugins ({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+
     return [
-        new webpack.ProgressPlugin(),
         new HtmlWebpackPlugin({
-            template: paths.html
+            template: paths.html,
         }),
+        new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css'
+            chunkFilename: 'css/[name].[contenthash:8].css',
         })
     ]
 }
