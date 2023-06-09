1dd2c41cc1c34e283bac8da87725f67277e3fc4c
ravelin9 <kvist1495@gmail.com>
2023-06-09 13:37:47 +0500

    add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,25 @@
 import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
 import "./styles/index.scss"
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Main page</Link>
-            <Link to={'/about'}>About</Link>
-            <Suspense fallback={<div>...Loading</div>}>
-                <Routes>
+          <Suspense fallback="">
+            <Navbar/>
+            <div className="content-page">
+              <Sidebar />
+              <AppRouter/>
+            </div>
+          </Suspense>
 
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-
-                </Routes>
-            </Suspense>
         </div>
     );
 };
