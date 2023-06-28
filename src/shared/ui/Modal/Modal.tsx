47777ca80819fbc3ef4b770fb2d01df807ec0085
ravelin9 <kvist1495@gmail.com>
2023-06-28 14:21:04 +0500

    add login, form ui, new input

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,9 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import React, {
-    ReactEventHandler, ReactNode, useCallback, useEffect, useRef, useState,
+    ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
 import { Portal } from 'shared/ui/Portal/Portal';
-import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 
 interface ModalProps{
@@ -11,6 +10,7 @@ interface ModalProps{
   children?: ReactNode,
   isOpen?: boolean,
   onClose?: () => void,
+    lazy?: boolean,
 }
 
 const ANIMATION_DELAY = 300;
@@ -20,10 +20,18 @@ export const Modal = (props: ModalProps) => {
         children,
         isOpen = false,
         onClose,
+        lazy,
     } = props;
 
     const [isClosing, setIsClosing] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true);
+        }
+    }, [isOpen]);
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -58,6 +66,10 @@ export const Modal = (props: ModalProps) => {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
     };
+
+    if (lazy && !isMounted) {
+        return null;
+    }
     return (
         <Portal>
             <div className={classNames(cls.Modal, mods, [className])}>
