32fd1daa98e5f910a0cb9758aabf354e49b617fc
ravelin9 <kvist1495@gmail.com>
2023-06-29 17:37:53 +0500

    add tests for login module, add screenshot tests, add visual tool for screenshot tests

---
+++ src/features/AuthByUsername/model/selectors/getLoginPassword/getLoginPassword.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginPassword } from './getLoginPassword';
+
+describe('getLoginPassword.test', () => {
+    test('should return password', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                password: '122',
+            },
+        };
+        expect(getLoginPassword(state as StateSchema)).toEqual('122');
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginPassword(state as StateSchema)).toEqual('');
+    });
+});
